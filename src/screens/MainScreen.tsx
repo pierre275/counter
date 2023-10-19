fc8e56da6d3f260be9bd4046ab4cf6bc5029f077
pierre <pbedlechowicz@gmail.com>
2023-10-19 15:48:33 +0200

    Connect redux increment with view

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import {CustomButton} from '../components/Button';
 import {CounterText} from '../components/CounterText';
 import {StyleSheet} from 'react-native';
+import {useAppSelector} from '../store/hooks/useAppSelector';
+import {useAppDispatch} from '../store/hooks/useAppDispatch';
+import {counterActions} from '../store/slices/counter';
 
 export const MainScreen = () => {
-  const handleIncrement = () => {};
+  const counter = useAppSelector(store => store.counter.value);
+  const dispatch = useAppDispatch();
+
+  const canIncrement = counter < 100;
+
+  const handleIncrement = () => {
+    dispatch(counterActions.INCREMENT());
+  };
   const handleDecrement = () => {};
 
   return (
     <>
-      <CounterText value={0} />
+      <CounterText value={counter} />
       <CustomButton
+        disabled={!canIncrement}
         title={'Increment'}
         onPress={handleIncrement}
         style={styles.incrementButton}
